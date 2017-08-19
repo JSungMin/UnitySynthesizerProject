85808f57e357f1e5a50b303d009d34c5489d1105
= <jsm1505104@gmail.com>
2017-08-20 00:41:02 +0900

    Sampling

---
+++ Assets/Scripts/SoundForker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundForker : MonoBehaviour {
+	public List<AudioMixerGroup> mixerList = new List<AudioMixerGroup> ();
+
+	public void SetAudioMixer (AudioSource[] aSources)
+	{
+		for (int i = 0 ; i < aSources.Length; i++)
+		{
+			aSources [i].outputAudioMixerGroup = mixerList [i];
+		}
+	}
+
+}
